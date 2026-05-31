1191136939985f3e244b46413f0d766d5605d93f
Relay <relay-lvs0@protonmail.com>
2026-05-31 13:28:32 +0200

    Update PWA v2.0 iPhone

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'liminal-v1';
+const ASSETS = ['/', '/index.html', '/game.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
